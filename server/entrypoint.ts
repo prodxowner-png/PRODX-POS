0fb6010bc5918eb0c31dfa935fc479474af4f94d
PRODX-OWNER <prodx.owner@gmail.com>
2026-09-24 09:53:42 +0700

    feat: wire production catalog read route

--- server/entrypoint.ts
+++ server/entrypoint.ts
@@ -5,6 +5,7 @@ import { createPostgresAuthorization } from './auth/production-authorization';
 import { asSqlExecutor, createPostgresPool } from './db/postgres';
 import { createTransactionalPostgresExecutor } from './db/transaction';
 import { registerCheckoutRoute } from './http/checkout-route';
+import { registerCatalogRoute } from './http/catalog-route';
 import { createApp } from './http/createApp';
 import { registerRefundRoute } from './http/refund-route';
 import { registerPaymentLifecycleRoute } from './http/payment-lifecycle-route';
@@ -26,6 +27,7 @@ export const createProductionApp = () => {
     },
     authorizeRequest: authorize,
     configureRoutes: (configuredApp) => {
+      registerCatalogRoute(configuredApp, sql);
       registerCheckoutRoute(configuredApp, transactions);
       registerRefundRoute(configuredApp, transactions);
       registerPaymentLifecycleRoute(configuredApp, transactions);
